2d7bb8370b0d90f06aabbf531b5100d64d818191
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-05-11 16:17:31 -0500

    instructions

--- localsettings.py
+++ localsettings.py
@@ -1,6 +1,8 @@
 TICKERS = [
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], 
+  # Khagesh stocks here inside brackets and comma after it [],
   
           ]
 EMAILS = ['williamjudge94@gmail.com',
+          #khagesh email here as a string and comma after it
          ]
