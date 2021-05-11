1a7a4cda22b80fca3034c567edc7772b75020664
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-05-11 16:14:54 -0500

    Updating multi-user

--- localsettings.py
+++ localsettings.py
@@ -1,1 +1,6 @@
-TICKERS = ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ']
+TICKERS = [
+  ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], 
+  
+          ]
+EMAILS = ['williamjudge94@gmail.com',
+         ]
