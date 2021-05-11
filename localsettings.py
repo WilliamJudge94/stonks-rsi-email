d310e65102ac5114c758a171e6e6e37e24407d64
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-05-11 16:28:15 -0500

    updates

--- localsettings.py
+++ localsettings.py
@@ -1,8 +1,8 @@
 TICKERS = [
-  ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], 
-  # Khagesh stocks here inside brackets and comma after it [],
+  ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Will Stonks
+  ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Khagesh Stonks
   
           ]
 EMAILS = ['williamjudge94@gmail.com',
-          #khagesh email here as a string and comma after it
+          'kkumar26@uic.edu',
          ]
