f077acfda23041139cd586a7674f28a78f38e6a4
md3286 <84092961+md3286@users.noreply.github.com>
2021-05-12 13:24:56 -0500

    Update localsettings.py

--- localsettings.py
+++ localsettings.py
@@ -1,8 +1,9 @@
 TICKERS = [
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Will Stonks
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Khagesh Stonks
-  
+  ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'TSM', 'DIS', 'DOGE-USD', 'SPY', 'QQQ'], # Mike Stonks
           ]
 EMAILS = ['williamjudge94@gmail.com',
           'kkumar26@uic.edu',
+          'mdato2@uic.edu',
          ]
