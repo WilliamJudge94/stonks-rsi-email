89368c0b082982e9d367add7a13a68902f824c72
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-09-17 09:58:25 -0500

    test update

--- localsettings.py
+++ localsettings.py
@@ -1,16 +1,16 @@
 TICKERS = [
-  #['AAPL', 'MSFT', 'AMZN', 'BABA', 'NVDA', 'TSLA', 'BTC-USD', 'ETH-USD', 'DOGE-USD', 'RYCEY', 'SAVE', 'SPY', 'QQQ'], # Will Stonks
-  #['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Khagesh Stonks
-  #['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'TSM', 'DIS', 'DOGE-USD', 'SPY', 'QQQ', 'AMD', 'INTC'], # Mike Stonks
-  #['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Alec Stonks
-  #['BP', 'O', 'MMM', 'LOAN', 'ACRE', 'T', 'COIN', 'AAPL', 'XOM', 'DOW', 'WFC', 'BAC', 'DIV', 'HPQ', 'KO', 'BTC-USD', 'ETH-USD'], # Tristan Stonks
+  ['AAPL', 'MSFT', 'AMZN', 'BABA', 'NVDA', 'TSLA', 'BTC-USD', 'ETH-USD', 'DOGE-USD', 'RYCEY', 'SAVE', 'SPY', 'QQQ'], # Will Stonks
+  ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Khagesh Stonks
+  ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'TSM', 'DIS', 'DOGE-USD', 'SPY', 'QQQ', 'AMD', 'INTC'], # Mike Stonks
+  ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Alec Stonks
+  ['BP', 'O', 'MMM', 'LOAN', 'ACRE', 'T', 'COIN', 'AAPL', 'XOM', 'DOW', 'WFC', 'BAC', 'DIV', 'HPQ', 'KO', 'BTC-USD', 'ETH-USD'], # Tristan Stonks
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Andrew Stonks
           ]
 EMAILS = [
-          #'williamjudge94@gmail.com',
-          #'kkumar26@uic.edu',
-          #'mdato2@uic.edu',
-          #'almalooley@gmail.com',
-          #'tristan.trautman@hotmail.com',
+          'williamjudge94@gmail.com',
+          'kkumar26@uic.edu',
+          'mdato2@uic.edu',
+          'almalooley@gmail.com',
+          'tristan.trautman@hotmail.com',
           'apmalooley@gmail.com',
          ]
