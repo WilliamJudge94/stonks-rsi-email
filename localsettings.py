ea31033b990c2e28fd2704bfbd9cb0b4534098bc
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-11-12 19:12:14 -0600

    added shiba

--- localsettings.py
+++ localsettings.py
@@ -1,9 +1,9 @@
 TICKERS = [
-  ['AAPL', 'MSFT', 'AMZN', 'BABA', 'KO', 'NVDA', 'TSLA', 'BTC-USD', 'ETH-USD', 'DOGE-USD', 'RYCEY', 'SAVE', 'SPY', 'QQQ'], # Will Stonks
+  ['AAPL', 'MSFT', 'AMZN', 'BABA', 'KO', 'NVDA', 'TSLA', 'BTC-USD', 'ETH-USD', 'SHIB-USD', 'DOGE-USD', 'RYCEY', 'SAVE', 'SPY', 'QQQ'], # Will Stonks
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Khagesh Stonks
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'TSM', 'DIS', 'DOGE-USD', 'SPY', 'QQQ', 'AMD', 'INTC'], # Mike Stonks
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Alec Stonks
-  ['BP', 'O', 'MMM', 'LOAN', 'ACRE', 'T', 'COIN', 'AAPL', 'XOM', 'DOW', 'WFC', 'BAC', 'DIV', 'HPQ', 'KO', 'BTC-USD', 'ETH-USD', 'RYCEY', 'INTC'], # Tristan Stonks
+  ['BP', 'O', 'MMM', 'LOAN', 'ACRE', 'T', 'COIN', 'AAPL', 'XOM', 'DOW', 'WFC', 'BAC', 'DIV', 'HPQ', 'KO', 'BTC-USD', 'ETH-USD', 'SHIB-USD', 'RYCEY', 'INTC'], # Tristan Stonks
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Andrew Stonks
   ['AAPL', 'MSFT', 'AMZN', 'BABA', 'KO', 'NVDA', 'TSLA', 'BTC-USD', 'ETH-USD', 'DOGE-USD', 'RYCEY', 'SAVE', 'SPY', 'QQQ'], # Caraline Stonks
           ]
