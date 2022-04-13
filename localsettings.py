5297059bee97850053c3548a6c03978000dd38be
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2022-04-13 14:04:45 -0500

    Removing MCMJ

--- localsettings.py
+++ localsettings.py
@@ -6,7 +6,7 @@ TICKERS = [
   ['BP', 'O', 'MMM', 'LOAN', 'ACRE', 'T', 'COIN', 'AAPL', 'XOM', 'DOW', 'WFC', 'BAC', 'DIV', 'HPQ', 'KO', 'BTC-USD', 'ETH-USD', 'SHIB-USD', 'RYCEY', 'INTC'], # Tristan Stonks
   ['AAPL', 'NVDA', 'TSLA', 'AMZN', 'BTC-USD', 'ETH-USD', 'RYCEY', 'SAVE', 'DOGE-USD', 'SPY', 'QQQ'], # Andrew Stonks
   ['AAPL', 'MSFT', 'AMZN', 'BABA', 'KO', 'NVDA', 'TSLA', 'BTC-USD', 'ETH-USD', 'DOGE-USD', 'RYCEY', 'SAVE', 'SPY', 'QQQ'], # Caraline Stonks
-  ['MCMJ', 'AAPL', 'MSFT', 'AMZN', 'KO', 'NVDA', 'TSLA', 'BTC-USD', 'ETH-USD', 'SPY', 'QQQ'], # Jake Stonks
+  ['AAPL', 'MSFT', 'AMZN', 'KO', 'NVDA', 'TSLA', 'BTC-USD', 'ETH-USD', 'SPY', 'QQQ'], # Jake Stonks
   ['SPY', 'QQQ'], # Tristan Friend 1
   ['SPY', 'QQQ'], # Tristan Friend 2
           ]
